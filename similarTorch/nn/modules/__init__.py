5025031c5979f13e435a6d81219eb0bec553b8f1
RayshineRen <1324789704@qq.com>
2024-03-28 22:01:22 +0800

    similarTorch-container

--- similarTorch/nn/modules/__init__.py
+++ similarTorch/nn/modules/__init__.py
@@ -2,6 +2,8 @@ from .linear import Linear
 from .init import kaiming_uniform_, uniform_, ones_, zeros_
 from .creation import zeros, ones, zeros_like, ones_like, empty, empty_like, rands
 from .activation import ReLU, Sigmoid, Softmax, Softplus, Softsign, ArcTan, Tanh
+from .container import Sequential
+from .flatten import Flatten
 from .loss import MSELoss, CrossEntropyLoss
 from .manipulation import SwapAxes, Reshape, GetItem
 from .mathematical import (
---
+++ similarTorch/nn/modules/container.py
@@ -0,0 +1,22 @@
+from .module import Module
+
+
+class Sequential(Module):
+    def __init__(self, *sequences):
+        super(Sequential, self).__init__()
+        self.module_list = list(sequences)
+        for i_mod, module in enumerate(self.module_list):
+            try:
+                module_state_dict = module.get_state_dict()
+                updated_names = [
+                    (f"sequential.{i_mod}.{k}", v) for k, v in module_state_dict.items()
+                ]
+                self.register_parameter(*updated_names)
+            except AttributeError:
+                pass
+
+    def forward(self, *input):
+        out = self.module_list[0](*input)
+        for module in self.module_list[1:]:
+            out = module(out)
+        return out
